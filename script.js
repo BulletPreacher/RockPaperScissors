9d77d73cd5ebbb6f98096e5ff0279d28a290eaf9
Lyle <Lyle.strydom96@gmail.com>
2022-11-28 19:58:21 +0200

    Add interface, clean up code

--- script.js
+++ script.js
@@ -1,80 +1,89 @@
+let computerDisplay = document.getElementById("computerScore");
+let playerDisplay = document.getElementById("playerScore");
+let messageDisplay = document.getElementById("displayMessage");
+let playerChoiceDisplay = document.getElementById("playerChoice");
+let computerChoiceDisplay = document.getElementById("computerChoice");
+
 function getComputerChoice() {
     const computerChoices = ["rock", "paper", "scissors"];
     let computerChoice = Math.floor(Math.random() * computerChoices.length);
     return computerChoices[computerChoice];
 }
 
-function validatePlayerChoice(x) {
-    while ((x !== "rock") && (x !== "paper") && (x !== "scissors")) {
-        x = prompt("Invalid please enter either Rock, Paper or Scissors").toLowerCase();
-    }
-    return x;
-}
+
+var submit = document.getElementById("submitChoice");
+
+
 
 var playerScore = 0;
 var computerScore = 0;
 
+
+function computerWinDisplay(a, b) {
+    messageDisplay.innerHTML = "Computer wins this round";
+    playerChoiceDisplay.innerHTML = "You Chose:" + a;
+    computerChoiceDisplay.innerHTML = "Computer Chose:" + b;
+    computerScore = computerScore + 1;
+    computerDisplay.innerHTML = "Computer Score: " + computerScore;
+    if(computerScore==5){
+        messageDisplay.innerHTML = "Computer won";
+        messageDisplay.style.fontSize = "xx-large";
+    }
+    
+}
+
+function playerWinDisplay(a, b) {
+    playerChoiceDisplay.innerHTML = "You Chose:" + a;
+    computerChoiceDisplay.innerHTML = "Computer Chose:" + b;
+    messageDisplay.innerHTML = "Player wins this round";
+    playerScore = playerScore + 1;
+    playerDisplay.innerHTML = "Player Score: " + playerScore;  
+    if (playerScore ==5){
+        messageDisplay.innerHTML = "Player won";
+        messageDisplay.style.fontSize = "xx-large"; 
+    }
+    
+}
+
+
 function playRound(choice1, choice2) {
     if (choice1 == "rock") {
-        if (choice2 == "rock") {
-            console.log("Draw");
-        } else if (choice2 == "paper") {
-            console.log("Computer wins this round");
-            computerScore = computerScore + 1;
-            document.getElementById("computerScore").innerText = "Computer Score: " + computerScore;
+        if (choice2 == "paper") {
+            computerWinDisplay(choice1, choice2);
         } else if (choice2 == "scissors") {
-            console.log("Player wins this round");
-            playerScore = playerScore + 1;
-            document.getElementById("playerScore").innerText = "Player Score: " + playerScore;
+            playerWinDisplay(choice1, choice2);
         }
     }
     if (choice1 == "paper") {
-        if (choice2 == "paper") {
-            console.log("Draw");
-        } else if (choice2 == "scissors") {
-            console.log("Computer wins this round");
-            computerScore = computerScore + 1;
-            document.getElementById("computerScore").innerText = "Computer Score: " + computerScore;
+        if (choice2 == "scissors") {
+            computerWinDisplay(choice1, choice2);
         } else if (choice2 == "rock") {
-            console.log("Player wins this round");
-            playerScore = playerScore + 1;
-            document.getElementById("playerScore").innerText = "Player Score: " + playerScore;
+            playerWinDisplay(choice1, choice2);
         }
     }
     if (choice1 == "scissors") {
-        if (choice2 == "scissors") {
-            console.log("Draw");
-        } else if (choice2 == "rock") {
-            console.log("Computer wins this round");
-            computerScore = computerScore + 1;
-            document.getElementById("computerScore").innerText = "Computer Score: " + computerScore;
+        if (choice2 == "rock") {
+            computerWinDisplay(choice1, choice2);
         } else if (choice2 == "paper") {
-            console.log("Player wins this round");
-            playerScore = playerScore + 1;
-            document.getElementById("playerScore").innerText = "Player Score: " + playerScore;
+            playerWinDisplay(choice1, choice2);
         }
     }
-}
-
-
-
-function game() {
-    playerScore = 0;
-    computerScore = 0;
-    while ((playerScore !== 5) && (computerScore !== 5)) {
-        let playerSelection = validatePlayerChoice(prompt("Rock, Paper or Scissors?").toLowerCase());
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log("PlayerScore: " + playerScore);
-        console.log("ComputerScore: " + computerScore);
-    } if (playerScore > computerScore) {
-        console.log("Player wins the game");
-
-    } else {
-        console.log("Computer wins the game");
+    if (choice1 == choice2) {
+        playerChoiceDisplay.innerHTML = "You Chose:" + choice1;
+        computerChoiceDisplay.innerHTML = "Computer Chose:" + choice2;
+        messageDisplay.innerHTML = "draw";
     }
 }
 
 
 
-game();
+
+submit.onclick = function game() {
+    x = document.getElementById("playerAnswer").value.toLowerCase();
+    y = getComputerChoice();
+    if ((x !== "rock") && (x !== "paper") && (x !== "scissors")) {
+        messageDisplay.innerHTML = "Please enter a valid option";
+    }
+    playRound(x, y);
+}
+
